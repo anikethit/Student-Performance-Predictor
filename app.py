643425630e5a8b3a5c258dda61aa371683381784
anikethit <aniketmaiti33@gmail.com>
2026-02-18 22:35:18 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pandas as pd
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -74,4 +75,6 @@ def predict():
 
 # Run Server
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
+
